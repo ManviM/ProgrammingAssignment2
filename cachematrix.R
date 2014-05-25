cbbd27d665517c01100ac66c2123db2133cdff49
ManviM <manvi.mohan1@gmail.com>
2014-05-25 20:15:46 +0530

    committing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Programming assignment2 involves two functions:
+##makeCacheMatrix: This function creates a special "matrix" object that can cache 
+##                 its inverse.
+##cacheSolve: This function computes the inverse of the special "matrix" returned by 
+##            makeCacheMatrix above. If the inverse has already been calculated 
+##            (and the matrix has not changed), then the cachesolve should retrieve 
+##            the inverse from the cache.
 
-## Write a short comment describing this function
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## makeCacheMatrix function calculates the inverse of input matrix 'x' and 
+## stores the input matrix 'x' and its inverse 'm' in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    y <<- x    
+    m <<- solve(x)
+    
 }
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## Write a short comment describing this function
+## cacheSolve function first checks if inverse of the input matrix is already calculated
+## If yes then it populates the value stored in cache
+## If not then it calculates the inverse freshly
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x=matrix(),...) {
+    
+    n<-matrix(nrow=nrow(m),ncol=ncol(m))
+    
+    ## Assign the matrix for which inverse is already calculated to 'orig'
+    orig<-y
+    
+    
+    ## identical(x,orig) finds if matrix input to cacheSolve function 
+    ##  is same as the original matrix for which inverse has been calculated
+    
+    ## is.matrix(m) checks if the m object found is a matrix only
+    ## identical(m,n)==FALSE   checks that m matrix is not null by comparing it with 'n' matrix
+    
+    if(identical(x,orig) & is.matrix(m) & identical(m,n)==FALSE) {
+        message("getting cached data")
+        return(m)
+    }
+    else {
+        ## if the input matrix to cacheSolve function is different from the 
+        ## one for which inverse was calculated then calculate the inverse matrix k
+        
+        k <- solve(x)
+        message("calculating inverse")
+        return(k)}
+}    
+
